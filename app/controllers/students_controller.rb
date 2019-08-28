707c105511653c1347d45371c7e4f396ab167ffd
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-08-28 16:56:04 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -14,6 +14,7 @@ class StudentsController < ApplicationController
   def create
     @student = Student.new
     @student.first_name = params[:student][:first_name]
+    @student.last_name = params[:student][:last_name]
   end
 
 end
